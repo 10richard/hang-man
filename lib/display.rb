d431e8e7c864d2375a814d7cf621bed9dfac4d41
Richard Ten <rten04@icloud.com>
2022-12-29 23:28:33 -0500

    Add/modify methods

--- lib/display.rb
+++ lib/display.rb
@@ -1,13 +1,21 @@
 module Display
 
     def display_difficulty(difficulty)
-        if difficulty == '1'
-            'EASY'
-        elsif difficulty == '2'
-            'MEDIUM'
-        elsif difficulty == '3'
-            'HARD'
-        else
-            'ERROR: CAN\'T DISPLAY DIFFICULTY'
+        {
+            '1' => 'EASY',
+            '2' => 'MEDIUM',
+            '3' => 'HARD'
+        }[difficulty]
+    end
+
+    def display_errors(error)
+        {
+            
+        }[error]
+
+    def display_banner
+        puts '###############################'
+        puts '       WELCOME TO HANGMAN      '
+        puts '###############################'
     end
 end
