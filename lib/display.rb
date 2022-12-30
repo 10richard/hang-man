152b87da155155b5472a7ec71171204d9206d8ee
Richard Ten <rten04@icloud.com>
2022-12-30 00:16:59 -0500

    Add/modify methods

--- lib/display.rb
+++ lib/display.rb
@@ -8,9 +8,17 @@ module Display
         }[difficulty]
     end
 
+    def display_instructions(option)
+        {
+            'get_guess' => 'Please enter a guess',
+        }
+    end
+
+    def display_word
+
     def display_errors(error)
         {
-            
+            'failed_pick' => 'You did not enter any of the given options',
         }[error]
 
     def display_banner
