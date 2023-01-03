06eee6d307e1c232a8ade73d837ba6ce4e41c18e
Richard Ten <rten04@icloud.com>
2023-01-02 23:18:27 -0500

    Finish functionality of game
    Game is 100% playable
    
    Removed the ability to guess word (unethical imo), user can only guess
    letters now
    
    *Just need to make it possible to save the game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,11 +1,12 @@
 require_relative 'display.rb'
+require 'json'
 
 class Hangman
     include Display
 
     def initialize(word)
         @guess = ''
-        @attempts_left = 7
+        @attempts_left = 10
         @secret_word = word.split('')
         @hidden_key = Array.new(@secret_word.length, '_')
         @valid_guess = false
@@ -17,10 +18,12 @@ class Hangman
     def play
         #will run all methods here
         until @game_over
+            puts
+            puts "Attempts left: #{display_attempts(@attempts_left)}"
             puts letter_guesses(@prev_guesses)
-            puts word_guesses(@prev_guesses)
             until @valid_guess
                 puts @hidden_key.join(' ')
+                puts
                 @guess = get_guess()
                 check_validity
             end
@@ -29,6 +32,7 @@ class Hangman
             @valid_guess = false
             end_game?
         end
+        puts @winner == true ? game_result('win', @secret_word) : game_result('lose', @secret_word)
     end
 
     def modify_key?
@@ -48,6 +52,7 @@ class Hangman
 
     def get_guess
         puts display_instructions('get_guess')
+        puts display_instructions('ask_save')
         gets.chomp
     end
 
@@ -56,26 +61,20 @@ class Hangman
         #checks if guess is in previously guessed list (if it is then ask for another guess)
         #if guess is a word, then it will check if it is a real english word (using google txt file)
         if @guess[/[a-zA-Z]+/] == @guess
-            if @guess.length > 1 && !@prev_guesses.include?(@guess)
-                if @guess.length == @secret_word.length
-                    word_file = File.open('google-10000-english-no-swears.txt', 'r')
-
-                    word_file = word_file.readlines
-
-                    if word_file.include?("#{@guess}\n")
-                        @valid_guess = true
-                    else
-                        puts display_errors('word_guess')
-                    end
-                elsif @guess.length < @secret_word.length
-                    puts display_errors('short_word')
-                elsif @guess.length > @secret_word.length
-                    puts display_errors('long_word')
-                end
-            elsif !@prev_guesses.include?(@guess)
+            if !@prev_guesses.include?(@guess) && @guess.length == 1
                 @valid_guess = true
+            elsif @guess == 'save'
+                validate_save
+            elsif @guess.length > 1 || @guess.length < 1
+                puts
+                puts display_errors('one_letter')
+                puts
             else
+                puts
                 puts display_errors('already_guessed')
+                puts
+                puts "Attempts left: #{display_attempts(@attempts_left)}"
+                puts letter_guesses(@prev_guesses)
             end
         else
             puts display_errors('not_letter')
@@ -91,7 +90,6 @@ class Hangman
             @winner = true
         elsif @secret_word.include?(@guess)
             @prev_guesses.push(@guess)
-
         else
             @prev_guesses.push(@guess)
             @attempts_left -= 1
@@ -103,11 +101,51 @@ class Hangman
             @game_over = true
         elsif @hidden_key.none?('_')
             @game_over = true
+            @winner = true
         end
     end
 
-    def save_info
+    def validate_save
+        valid_answers = ['y', 'n']
+        puts display_instructions('save_game?')
+        answer = gets.chomp
+        until valid_answers.include?(answer)
+            puts display_errors('failed_pick')
+            puts 'Enter y or n'
+            answer = gets.chomp
+        end
+
+        if answer == 'y'
+            save_game
+        elsif answer == 'n'
+            return
+        else
+            puts 'What did you do >:('
+        end
+    end
+
+    def save_game
         #will serialize: previously guess list, attempts left, and hidden word
         #tbh have 0 clue what I will do here, but we move :)
+        
+    end
+
+    def to_json
+        JSON.dump ({
+            guess: @guess,
+            attempts_left: @attempts_left,
+            secret_word: @secret_word,
+            hidden_key: @hidden_key,
+            prev_guesses: @prev_guesses
+        })
+    end
+
+    def load_json(string)
+        data = JSON.load(string)
+        @guess = data[:guess]
+        @attempts_left = data[:attempts_left]
+        @secret_word = data[:secret_word]
+        @hidden_key = data[:hidden_key]
+        @prev_guesses = data[:prev_guesses]
     end
 end
